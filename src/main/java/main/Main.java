034c09b541e0c8d2bf88d31d8e6559579af2707a
nellrun <daniil.shchesnyak@gmail.com>
2016-12-09 20:41:20 +0300

    deleted useless classes

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -15,7 +15,6 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-//        AccountService accountService = new AccountService();
 
         HashMap<String, String> sessionToLogin = new HashMap<String, String>();
 
